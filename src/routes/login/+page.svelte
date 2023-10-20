93f5ee330b398ffe0c7f6fa46d7683e53ceab929
caiordm <caiordm08@gmail.com>
2023-10-19 23:48:42 -0300

    feat: adding login and register

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,118 @@
+<script>
+	let email = "";
+	let password = "";
+
+	async function login(){
+
+	}
+
+</script>
+
+<section>
+	<div class="title">
+		<h1>Encurtador de URL</h1>
+		<h2>Faça seu login!</h2>
+	</div>
+	<form on:submit={login}>
+		<div class="box">
+			<label for="email">Email: </label>
+			<input type="email" name="email" class="input" bind:value={email} placeholder="Digite seu email">
+		</div>
+		<div class="box">
+			<label for="password">Senha: </label>
+			<input type="password" name="password" class="input" bind:value={password} placeholder="Digite sua senha">
+		</div>
+		<button type="submit">Entrar</button>
+		<a href="/signup">Fazer cadastro!</a>
+	</form>
+</section>
+
+<style>
+	section {
+		width: 100%;
+		height: 60vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 2rem;
+	}
+
+	form {
+		width: 20%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: start;
+		gap: 0.8rem;
+		padding: 1rem;
+
+		background: rgba(255, 255, 255, 0.25);
+		backdrop-filter: blur(8px);
+		-webkit-backdrop-filter: blur(8px);
+		border-radius: 8px;
+		border: 2px solid #25262735;
+	}
+
+	input {
+		padding: 0.6rem;
+		font-size: 12pt;
+		border: 2px solid #cccccc;
+		border-radius: 8px;
+	}
+
+	input:focus {
+		outline: none;
+		border: 2px solid #fa5700;
+	}
+
+	h1 {
+		font-size: 36pt;
+		color: #fa5700;
+		margin: 0;
+	}
+
+	h2 {
+		margin: 0;
+		color: #444444;
+	}
+
+	label {
+		font-size: 14pt;
+		font-weight: 500;
+		color: #444444;
+	}
+
+	button {
+		font-size: 12pt;
+		padding: 0.4rem 1.8rem;
+		border: 2px solid #e65000;
+		color: #fff;
+		font-weight: bold;
+		background-color: #fa5700;
+		border-radius: 6px;
+		transition: all 0.2s;
+	}
+
+	button:hover {
+		background-color: #e65000;
+		cursor: pointer;
+	}
+
+	a {
+		color: #fa5700;
+	}
+
+	.title {
+		display: flex;
+		flex-direction: column;
+		gap: 0;
+	}
+	
+	.box {
+		display: flex;
+		flex-direction: column;
+		gap: 0.4rem;
+	}	
+	
+</style>
